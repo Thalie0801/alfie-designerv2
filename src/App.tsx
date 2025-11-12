af523c3578b04b8164d1d56deafec5c416ed5b08
Kronys <nathaliestaelens@gmail.com>
2025-11-13 00:14:17 +0100

    Add chat widget to app layout (#327)

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ import ActivateAccess from "./pages/onboarding/Activate";
 import { AlfieChat } from "./components/AlfieChat"; // si tu gardes /chat
 import { AppLayoutWithSidebar } from "./components/AppLayoutWithSidebar";
 import { ChatGenerator } from "@/features/studio";
+import ChatWidget from "./components/chat/ChatWidget";
 
 const queryClient = new QueryClient();
 
@@ -234,6 +235,7 @@ const App = () => (
     <AuthProvider>
       <TooltipProvider>
         <AppRoutes />
+        <ChatWidget />
       </TooltipProvider>
     </AuthProvider>
   </QueryClientProvider>
